dd5c9d55eda495b446d4fabe9708793a1230dccc
jdeex <jdeex05@gmail.com>
2015-02-05 17:24:57 +0100

    modificada la URL de acceso

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -15,7 +15,7 @@ public class Main
 {
     // Base URI the Grizzly HTTP server will listen on
 //    public static final String BASE_URI = "http://localhost:8080/myapp/";
-    public static final String BASE_URI = "http://jdeex-heroku-first.herokuapp.com/myapp";
+    public static final String BASE_URI = "http://jdeex-heroku-rest.herokuapp.com/myapp";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
